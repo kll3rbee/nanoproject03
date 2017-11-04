0f0ea678299a02dea17f1a2b17e989a65d859fa8
kll3rbee <32694575+kll3rbee@users.noreply.github.com>
2017-11-05 06:26:37 +0800

    initial thought

---
+++ codeyourquiz.py
@@ -0,0 +1,22 @@
+easyq = '''Earth is our home planet. Scientists believe Earth and its ___1___ formed around the same
+time as the rest of the solar system. They think that was about 4.5 ___2___ years ago.
+Earth is the fifth-largest planet in the solar system. Its diameter is about 8,000 miles.
+And Earth is the third-closest planet to the sun. Its average ___3___ from the sun is
+about 93 million miles. Only Mercury and ___4___ are closer.'''
+
+mediumq = '''Despite the common conceptions of deserts as dry and hot, there are ___1___ deserts as well.
+The largest hot desert in the world, northern Africa's ___2___, reaches temperatures of 
+up to 122 degrees Fahrenheit (50 degrees Celsius) during the ___3___. But some deserts are
+always ___1___, like the ___4___ desert in Asia and the desert on the continent of Antarctica.
+Others are mountainous. Only about 10 percent of deserts are covered by ___5___ dunes.'''
+
+hardq = '''___1___ was the first national park ever to exist, designated in ___2___. Its status
+sparked an idea that ___3___ across the country and then across the world. National parks
+were spaces that human kind deemed ___4___ and worth protecting. ___5___, Mount Rainier,
+Wind Cave and Mesa Verde all gained status too, until eventually, in 1916, the National
+Parks System was created. One ___6___ charged with overseeing all aspects of these wildernesses.'''
+
+easyans = ['moon', 'billion', 'distance', 'Venus']
+mediumans = ['cold', 'Sahara', 'day', 'Gobi', 'sand' ]
+hardans = ['Yellowstone', '1872', 'spread', 'precious', 'Yosemite', 'entity']
+maxtries = 3
