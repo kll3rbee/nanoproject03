6cf7b1503386c187c70b0240698f8158a1612d99
kll3rbee <32694575+kll3rbee@users.noreply.github.com>
2017-11-09 01:18:51 +0800

    fix recommendation of the reviewer

--- codeyourquiz.py
+++ codeyourquiz.py
@@ -5,13 +5,13 @@ Earth is the fifth-largest planet in the solar system. Its diameter is about 8,0
 And Earth is the third-closest planet to the sun. Its average ___3___ from the sun is
 about 93 million miles. Only Mercury and ___4___ are closer.'''
 mediumq = '''Despite the common conceptions of deserts as dry and hot, there are ___1___ deserts as well.
-The largest hot desert in the world, northern Africa's ___2___, reaches temperatures of 
-up to 122 degrees Fahrenheit (50 degrees Celsius) during the ___3___. But some deserts are
-always ___1___, like the ___4___ desert in Asia and the desert on the continent of Antarctica.
+The largest hot desert in the world, northern Africa's ___2___ , reaches temperatures of 
+up to 122 degrees Fahrenheit (50 degrees Celsius) during the ___3___ . But some deserts are
+always ___1___ , like the ___4___ desert in Asia and the desert on the continent of Antarctica.
 Others are mountainous. Only about 10 percent of deserts are covered by ___5___ dunes.'''
-hardq = '''___1___ was the first national park ever to exist, designated in ___2___. Its status
+hardq = '''___1___ was the first national park ever to exist, designated in ___2___ . Its status
 sparked an idea that ___3___ across the country and then across the world. National parks
-were spaces that ___4___ deemed precious and worth protecting. ___5___, Mount Rainier,
+were spaces that ___4___ deemed precious and worth protecting. ___5___ , Mount Rainier,
 Wind Cave and Mesa Verde all gained status too, until eventually, in 1916, the National
 Parks System was created. One ___6___ charged with overseeing all aspects of these wildernesses.'''
 
